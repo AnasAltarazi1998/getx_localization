c8c5bf81bb9c797337b3c73d48aa41f8d8d5cda4
anas_altarazi <anas.anas1998,tar@gmail.com>
2021-01-20 06:05:06 -0800

    fix import issues

---
+++ getx_localization/lib/home_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_localization/controller/app_language.dart';
+
+/// local saved data
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String _selectedLang = 'en';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.all(20),
+        child: Directionality(
+          textDirection:
+              _selectedLang == 'en' ? TextDirection.ltr : TextDirection.rtl,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "login".tr,
+                style: TextStyle(fontSize: 32),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                "SignUp".tr,
+                style: TextStyle(fontSize: 32),
+              ),
+              GetBuilder<AppLanguage>(
+                init: AppLanguage(),
+                builder: (controller) {
+                  return DropdownButton(
+                    items: [
+                      DropdownMenuItem(
+                        child: Text("en"),
+                        value: 'en',
+                      ),
+                      DropdownMenuItem(
+                        child: Text("ar"),
+                        value: 'ar',
+                      ),
+                    ],
+                    value: controller.appLocale,
+                    onChanged: (value) {
+                      controller.changeLanguage(value);
+                      Get.updateLocale(Locale(value));
+                    },
+                  );
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
